d30c6c250a475ef4326b9cdb63b5606f9fc764d4
Aftab <aftab.khateeb@energyq.com.au>
2022-04-10 01:53:27 +1000

    Added screen shots

--- Tests/MarsTests.cs
+++ Tests/MarsTests.cs
@@ -5,33 +5,33 @@ using MarsQAProject.Pages;
 
 namespace MarsQAProject
 {
-    [TestFixture]
-    internal class MarsTests: CommonDriver
-    {     
+    //[TestFixture]
+    //internal class MarsTests: CommonDriver
+    //{     
 
 
-        [Test, Order(1), Description("Create")]
-        public void CreateFunction()
-        {
-            //MarsLoginPage loginobj = new MarsLoginPage();
-            //loginobj.LoginPage();
+    //    [Test, Order(1), Description("Create")]
+    //    public void CreateFunction()
+    //    {
+    //        //MarsLoginPage loginobj = new MarsLoginPage();
+    //        //loginobj.LoginPage();
 
-            //MProfilelanguagePage langobj = new MProfilelanguagePage();
-            //langobj.AddLanguge(Driver);
-        }
+    //        //MProfilelanguagePage langobj = new MProfilelanguagePage();
+    //        //langobj.AddLanguge(Driver);
+    //    }
 
-        [Test, Order(2), Description("Edit")]
-        public void EditFunction()
-        {
+    //    [Test, Order(2), Description("Edit")]
+    //    public void EditFunction()
+    //    {
              
-        }
+    //    }
 
-        [Test, Order(3), Description("Delete")]
-        public void DeleteFunction()
-        {
+    //    [Test, Order(3), Description("Delete")]
+    //    public void DeleteFunction()
+    //    {
             
-        }
+    //    }
 
 
-    }
+    //}
 }
